3eca3c417934a7003e1e92c315ae92117fad755c
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2024-10-13 01:03:18 +0500

    initilize the functions

--- app.c
+++ app.c
@@ -2,6 +2,56 @@
 
 int main()
 {
-    printf("Hello World");
+    int choice;
+    do
+    {
+        printf("   \nLibrary Managment System\n");
+        printf("1: Add Book \n");
+        printf("2: View All Books \n");
+        printf("3: Edit Book \n");
+        printf("4: Delete Book \n");
+        printf("5: Issue Book \n");
+        printf("6: View Issue Books \n");
+        printf("7: Search Book \n");
+        printf("0: Exit Program \n");
+        printf("Enter your choice: \n");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            addBook();
+            break;
+        case 2:
+            /* viewBooks(); */
+            break;
+        case 3:
+            /* editBooks(); */
+            break;
+        case 4:
+            /* deleteBook(); */
+            break;
+        case 5:
+            /* issueBook(); */
+            break;
+        case 6:
+            /* viewIssueBooks(); */
+            break;
+        case 7:
+            /* searchBook(); */
+            break;
+        case 0:
+            printf("Exiting....");
+            break;
+        default:
+            printf("Invalid Choice \n");
+        }
+    } while (choice != 0);
+
+    return 0;
+}
+
+int addBook()
+{
+    printf("Book Added");
     return 0;
 }
